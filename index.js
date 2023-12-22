b9d7f8f492e46ac0695f68637aa2cbc25cb3c03a
tarik <mahimtk2@gmail.com>
2023-12-22 11:12:11 +0600

    added

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
+const SSLCommerzPayment = require('sslcommerz-lts')
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASSWORD
+const is_live = false //true for live, false for sandbox
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.b6dwlgl.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -36,14 +38,58 @@ async function run() {
           const products = await productCollection.find().toArray();
           res.send(products)
       })
+    
+    app.get("/customer/checkout", (req, res) => {
+      const data = {
+        total_amount: 100,
+        currency: "BDT",
+        tran_id: "REF123", // use unique tran_id for each api call
+        success_url: "http://localhost:3030/success",
+        fail_url: "http://localhost:3030/fail",
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: "Customer Name",
+        cus_email: "customer@example.com",
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.redirect(GatewayPageURL);
+        console.log("Redirecting to: ", GatewayPageURL);
+      });
+    });
 
-    app.post("/customer/profile/address", async (req, res) => {
+    app.post("/customer/profile/address/", async (req, res) => {
       const reqBody = req.body;
       const result = await addressCollection.insertOne(reqBody);
       res.send(result);
     });
-    app.get("/customer/profile/address", async (req, res) => { 
-      const address = await addressCollection.find().toArray();
+    app.get("/customer/profile/address/:id", async (req, res) => { 
+      const getId = req.params.id;
+      const query = { _id: getId };
+      
+      const address = await addressCollection.findOne(query);
+      console.log(address);
       res.send(address);
     });
     
